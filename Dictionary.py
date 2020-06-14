6538b35ca92bf7c8d0f86fdadd6ad5f3af8e5d1a
ThomasMott <66922475+ThomasMott@users.noreply.github.com>
2020-06-14 21:41:46 +0100

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,6 +1,8 @@
-fish = ()
+#finding birthday from dictionary based on input
+
+find = ()
 birthday_dic = {
     "Albert Einstein" : "01/17/1776", "Benjamin Franklin" : "01/17/1876","Ada Lovelace": "01/17/1976"}
 
-fish = input(str("Who's birthday do you want to look up?"))
+find = input(str("Who's birthday do you want to look up?"))
 print(birthday_dic[fish])
